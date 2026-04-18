1a3021260b5ae73e85068b11deb15fe6f04aac2a
modongling <donglingmo93@gmail.com>
2026-04-18 13:09:12 +0800

    fix vitepress config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
+  lang: 'zh-CN',
   title: 'Dongling Blog',
   description: '学习记录与技术博客',
-  base: '/my-tech-blog/', // 这里改成你的仓库名
-  lang: 'zh-CN',
+  base: '/my-tech-blog/',
   themeConfig: {
     nav: [
       { text: '首页', link: '/' }
